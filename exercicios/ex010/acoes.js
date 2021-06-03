0149bfd9b444ff2096bd88156261e0fad69f9c69
Arê Nascimento <83874397+agnr-gab@users.noreply.github.com>
2021-06-03 00:13:39 -0300

    arquivos de exercícios

---
+++ exercicios/ex010/acoes.js
@@ -0,0 +1,16 @@
+function action01() {
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML ='<p>Clicaste no primeiro botão</p>'
+} 
+function action02() {
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML ='<p>Clicaste no segundo botão</p>'
+} 
+function action03() {
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML ='<p>Clicaste no terceiro botão</p>'
+} 
+function action04() {
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML ='<p>Clicaste no quarto botão</p>'
+} 
