25a0c3eb8fbdb5d10350f2f5c7381a617902fcab
Arê Nascimento <83874397+agnr-gab@users.noreply.github.com>
2021-06-02 21:55:04 -0300

    exercícios
    Atividades realizadas durante as aulas de fixação de exercícios parte 01 e 02.
    Site "Horários dos dia" com alteração de imagens

---
+++ exercicio14/script.js
@@ -0,0 +1,21 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = ` Agora são ${hora} horas! `
+    if (hora >= 0 && hora < 12){
+        document.body.style.background = '#6bb5c8'
+        msg.innerHTML += 'BOM DIA!'
+        img.src = 'foto-manha.png'
+    }else if (hora >=12 && hora < 18){
+        msg.innerHTML += 'BOA TARDE!'
+        img.src = 'foto-tarde.png'
+        document.body.style.background = '#f78518'
+    }else {
+        msg.innerHTML += 'BOA NOITE!'
+        img.src = 'foto-noite.png'
+        document.body.style.background = '#59476f'
+    }
+} 
