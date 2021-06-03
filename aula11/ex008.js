6f0ff21289af51a94cdd97d641d1cf59dfa05740
Arê Nascimento <83874397+agnr-gab@users.noreply.github.com>
2021-06-03 00:14:53 -0300

    add files via upload

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel > 60) {
+    console.log ('Você ultrpassou a velocidade permitida. MULTADO!')
+}
+console.log(`Dirija sempre usando o cinto de seguraça`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Cuba'
+console.log(`Vivendo em ${pais}`)
+if (pais !='Brasil') {
+console.log('Você é estrangeira(o)')
+}else{
+    console.log('Você é brasileira(o)')
+}
